2e606f7099576f01a1be4505a0eddfb3547c4f4a
wilmer <wilmer.arango.a@gmail.com>
2023-10-23 02:05:57 +0200

    Prodotto

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,14 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+// Nello stesso package aggiungete una classe Main con metodo main nella quale create tre prodotti e ne stampate il nome completo e il prezzo comprensivo di iva.
+// BONUS: nella classe Prodotto create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+public class Main {
+    public static void main(String[] args) {
+        Prodotto prodotto1 = new Prodotto("Prodotto1 ", "Descrizione1", 10, 0.22);
+        Prodotto prodotto2 = new Prodotto("Prodotto2 ", "Descrizione2", 20, 0.22);
+        Prodotto prodotto3 = new Prodotto("Prodotto3 ", "Descrizione3", 30, 0.22);
+
+        System.out.println("Prodotto 1: " + prodotto1.getNomeEsteso() + "Prezzo con Iva: " + prodotto1.getPrezzoConIva());
+        System.out.println("Prodotto 2: " + prodotto2.getNomeEsteso() + "Prezzo con Iva: " + prodotto2.getPrezzoConIva());
+        System.out.println("Prodotto 3: " + prodotto3.getNomeEsteso() + "Prezzo con Iva: " + prodotto3.getPrezzoConIva());
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,17 +1,73 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Ctrl+. with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package org.lessons.java.shop;
 
-        // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+import java.util.Random;
 
-            // Press F5 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing F9.
-            System.out.println("i = " + i);
-        }
+// Nel package org.lessons.java.shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+// Un prodotto è caratterizzato da:
+// - codice (numero intero)
+// - nome
+// - descrizione
+// - prezzo
+// - iva
+// Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+// alla creazione di un nuovo prodotto venga passato il valore di tutti gli attributi, tranne il codice, che deve essere valorizzato con un numero random
+// Il codice prodotto sia accessibile solo in lettura
+// Gli altri attributi siano accessibili sia in lettura che in scrittura
+// Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+// Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+// Nello stesso package aggiungete una classe Main con metodo main nella quale create tre prodotti e ne stampate il nome completo e il prezzo comprensivo di iva.
+// BONUS: nella classe Prodotto create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+public class Prodotto {
+        private final int codice;
+        private String  nome;
+        private String  descrizione;
+        private int prezzo;
+        private double iva;
+
+
+    public Prodotto(String nome, String descrizione, int prezzo, double iva) {
+        this.codice = generateRandomCode();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+    public int getCodice() {
+        return codice;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescrizione(){
+        return descrizione;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    public int getPrezzo() {
+        return prezzo;
+    }
+    public void setPrezzo(int prezzo) {
+        this.prezzo = prezzo;
+    }
+    public double getIva() {
+        return iva;
+    }
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+    public double getPrezzoConIva() {
+        return prezzo + prezzo  * iva;
+    }
+    public String getNomeEsteso() {
+        return String.format("%08d-%s", codice, nome);
+    }
+    public int generateRandomCode() {
+        Random random = new Random();
+        return random.nextInt(100000000);
     }
 }
+
